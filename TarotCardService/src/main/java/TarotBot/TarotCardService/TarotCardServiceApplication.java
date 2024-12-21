c8a5d01096056c5fe03e4b4c1aa3d2b2e0bacfcf
Евгения <ms.evgeniyarudakova@gmail.com>
2024-12-21 09:45:00 +0300

    Add files via upload

---
+++ TarotCardService/src/main/java/TarotBot/TarotCardService/TarotCardServiceApplication.java
@@ -0,0 +1,13 @@
+package TarotBot.TarotCardService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotCardServiceApplication.class, args);
+	}
+
+}
